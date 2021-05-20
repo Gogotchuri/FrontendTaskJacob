5e48310c154dca9e721275adc57223f06fe39449
Gogotchuri <igogo17@freeuni.edu.ge>
2021-05-20 20:20:43 +0400

    Added instructions and locale.js

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,17 +1,73 @@
 <template>
-  <div>
-    <validation-observer ref="observer" v-slot="{}">
-      <validation-provider name="email" rules="required|email" v-slot="{errors}">
-        <label>
-          Email
-          <input name="email" type="text" v-model="settings.email">
-        </label>
-        <div v-for="e in errors" :key="e">
-          {{e}}
-        </div>
-      </validation-provider>
-      <button @click="saveSettings">Submit</button>
-    </validation-observer>
+  <div class="container">
+    <div class="row text-center justify-content-center my-5">
+      <div class="col col-2">
+        <select v-model="country">
+          <option value="Georgia">Georgia</option>
+          <option value="Philippines">Philippines</option>
+          <option value="India">India</option>
+        </select>
+      </div>
+      <div>
+        Today: {{today}}
+      </div>
+    </div>
+    <div class="row text-center justify-content-center">
+      <div class="col col-3">
+        <validation-observer ref="observer" v-slot="{}">
+          <validation-provider name="email" rules="required|email" v-slot="{errors}">
+            <label>
+              Email
+              <input name="email" type="text" v-model="settings.email">
+            </label>
+            <div class="text-danger" v-for="e in errors" :key="e">
+              {{e}}
+            </div>
+          </validation-provider>
+          <validation-provider name="name" rules="required" v-slot="{errors}">
+            <label>
+              Name
+              <input name="name" type="text" v-model="settings.name">
+            </label>
+            <div class="text-danger" v-for="e in errors" :key="e">
+              {{e}}
+            </div>
+          </validation-provider>
+          <validation-provider name="ID" rules="" v-slot="{errors}">
+            <label>
+              Personal ID
+              <input name="ID" type="text" v-model="settings.personalID">
+            </label>
+            <div class="text-danger" v-for="e in errors" :key="e">
+              {{e}}
+            </div>
+          </validation-provider>
+          <validation-provider name="VAT" rules="" v-slot="{errors}">
+            <label>
+              VAT
+              <select name="vat" v-model="settings.isVATPayer">
+                <option :value="true">Yes</option>
+                <option :value="false">No</option>
+              </select>
+            </label>
+            <div class="text-danger" v-for="e in errors" :key="e">
+              {{e}}
+            </div>
+          </validation-provider>
+          <validation-provider v-if="settings.isVATPayer" name="VAT amount" rules="" v-slot="{errors}">
+            <label>
+              VAT Amount
+              <input type="number" v-model="settings.VATValue">
+            </label>
+            <div class="text-danger" v-for="e in errors" :key="e">
+              {{e}}
+            </div>
+          </validation-provider>
+          <br>
+          <button @click="saveSettings">Submit</button>
+        </validation-observer>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,13 +77,22 @@ export default {
   name: 'Settings',
   data () {
     return {
+      country: 'Georgia',
+      today: '',
       settings: {
-        email: ''
+        email: '',
+        name: '',
+        personalID: '',
+        isVATPayer: false,
+        VATValue: 0
       }
     };
   },
   components: {
     ValidationObserver, ValidationProvider
+  },
+  mounted () {
+    this.today = new Date();
   },
   methods: {
     async saveSettings () {
